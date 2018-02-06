b4f95fcfd7175f8c096e4fff0a0aa752fb2c3984
Omar Ihmoda <omarihmoda@DN51v59l.SUNet>
2018-02-06 11:31:31 -0800

    power level

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  numbers: number[];
+  startval: number;
+  powers = {
+    human: 1,
+    saiyan: 10,
+    supersaiyan: 90,
+    supersaiyantwo: 150,
+    supersaiyanthree: 250,
+    supersaiyanfour: 500
+  }
+  constructor(){
+    this.numbers = this.buildarray();
+  }
+
+  buildarray(){
+    let n_arr = []
+    for (let i = 1; i<=100; i++){
+      n_arr.push(i);
+    }
+    return n_arr;
+  }
+
+  calculatePowers(){
+    if (!this.startval){
+      this.startval = 1;
+    }
+    this.powers.human = 1;
+    this.powers.saiyan = 10;
+    this.powers.supersaiyan = 90;
+    this.powers.supersaiyantwo = 150;
+    this.powers.supersaiyanthree = 250;
+    this.powers.supersaiyanfour = 500;
+    for(let power in this.powers){
+        this.powers[power] *= this.startval;
+    }
+
+    console.log(this.powers.human);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/Forms';
+import { HttpModule } from '@angular/http';
 
 
 import { AppComponent } from './app.component';
+import { HumanComponent } from './human/human.component';
+import { SaiyanComponent } from './saiyan/saiyan.component';
+import { SupersaiyanComponent } from './supersaiyan/supersaiyan.component';
+import { SupersaiyantwoComponent } from './supersaiyantwo/supersaiyantwo.component';
+import { SupersaiyanfourComponent } from './supersaiyanfour/supersaiyanfour.component';
+import { SupersaiyanthreeComponent } from './supersaiyanthree/supersaiyanthree.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HumanComponent,
+    SaiyanComponent,
+    SupersaiyanComponent,
+    SupersaiyantwoComponent,
+    SupersaiyanfourComponent,
+    SupersaiyanthreeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
